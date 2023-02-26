6803be09b8cf7aa0298a3cde22d859aa211a0713
orpd0523 <orpd0523@gmail.com>
2023-02-25 18:51:59 -0600

    random shuffle

--- scripts/index.js
+++ scripts/index.js
@@ -6,53 +6,69 @@ fetch 6 pokemon images
 path in api: sprites.other.official-artwork
 set ids for every card(12)
 
+git card image tag w/ src
+
 pick pokemon images at random 
 
 
 card flips on click
 after click, card rotates
 once rotated, card shows pokemon image
-git card image tag w/ src
 will track which card to which image through id
 activate card rendering with event listener
 */
 
 const URL = "https://pokeapi.co/api/v2/pokemon";
 
-const image = [
-    {
-        id: 1,
-        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-    },
-    {
-        id: 2,
-        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
-    },
-    {
-        id: 3,
-        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"
-    },
-    {
-        id: 4,
-        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-    },
-    {
-        id: 5,
-        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"
-    },
-    {
-        id: 6,
-        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
-    },
-]
+const pokemon = [
+  {
+    id: 1,
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    id: 2,
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+  },
+  {
+    id: 3,
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+  },
+  {
+    id: 4,
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    id: 5,
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
+  },
+  {
+    id: 6,
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+  },
+];
 
+const image_base_url =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
 
+let random = [1, 2, 3, 4, 5, 6];
+random = shuffle([...random, ...random]);
 
-axios.get(URL + '/6')
-  .then(function (response) {
-    console.log(response.data.sprites.other["official-artwork"].front_default);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  
+function shuffle (arr) {
+ return arr.sort(() => Math.random() - 0.5);
+};
+
+const flipCards = document.querySelectorAll(".main__flip");
+
+flipCards.forEach((flipCard, i) => {
+  flipCard.addEventListener("click", (e) => {
+    const src = image_base_url + random[i] + ".png";
+    const image = flipCard.querySelector(".main__image");
+    image.src = src;
+  });
+});
